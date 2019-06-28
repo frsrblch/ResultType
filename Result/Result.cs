7ba5545a97648dde4a383231e890f3425102b887
Fraser Balch <farseer.ulwithy@gmail.com>
2019-06-28 12:02:30 -0600

    Add project files.

---
+++ Result/Result.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Result
+{
+    public static class Result
+    {
+
+    }
+
+    public readonly struct Result<TValue, TError>
+    {
+        public bool IsOkay { get; }
+        public bool IsError => !IsOkay;
+    }
+}
